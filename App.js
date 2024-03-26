a92d7e36e9c0455166ffe905c5621f6cbb86bcb4
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-26 16:09:37 -0400

    Props in react

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import logo from "./images/logo.webp";
 
 const Header = () => {
@@ -29,22 +29,20 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="restro-container">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        <RestaurantCard
+          resName="The Burger Club"
+          cusine="Burger, Pizza, Asian"
+        />
+        <RestaurantCard resName="KFC" cusine="Fried Chicken, Nuggets" />
       </div>
     </div>
   );
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  //{ resName, cusine } in the parameters OR
+  const { resName, cusine } = props; // Destructuring
+
   return (
     <div className="restro-card" style={styleCard}>
       <img
@@ -52,8 +50,8 @@ const RestaurantCard = () => {
         alt="Burger"
         src="https://b.zmtcdn.com/data/pictures/chains/3/17953943/816872e9f1fcc43304e21a0f050f5f21_o2_featured_v2.jpg?output-format=webp"
       />
-      <h3>The Burger Club</h3>
-      <h4>Burger, Pizza, Asian</h4>
+      <h3>{resName}</h3>
+      <h4>{cusine}</h4>
       <h4>4.4 stars</h4>
       <h4>38 minutes</h4>
     </div>
