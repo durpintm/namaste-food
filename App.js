7cc2b40e7cc7f38ddd2d9b6c03b9e77061c32fd7
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-26 00:18:42 -0400

    Add body and restro card component

--- App.js
+++ App.js
@@ -20,10 +20,34 @@ const Header = () => {
   );
 };
 
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restro-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="restro-card" style={styleCard}>
+      <h3>Namaste Food</h3>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
