49cf36ee71ebde67845025c458fccfa2d222f273
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-31 17:38:29 -0400

    Adding swiggy api data and displaying it in cards

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import logo from "./images/logo.webp";
+import restroList from "./swiggyapi";
 
 const Header = () => {
   return (
@@ -29,31 +30,49 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="restro-container">
-        <RestaurantCard
-          resName="The Burger Club"
-          cusine="Burger, Pizza, Asian"
-        />
-        <RestaurantCard resName="KFC" cusine="Fried Chicken, Nuggets" />
+        {/* <RestaurantCard restroData={restroList[0]} />
+        <RestaurantCard restroData={restroList[1]} />
+        <RestaurantCard restroData={restroList[2]} />
+        <RestaurantCard restroData={restroList[3]} />
+        <RestaurantCard restroData={restroList[4]} />
+        <RestaurantCard restroData={restroList[5]} />
+        <RestaurantCard restroData={restroList[6]} />
+        <RestaurantCard restroData={restroList[7]} />
+        <RestaurantCard restroData={restroList[8]} /> */}
+        {restroList.map((restro) => {
+          return <RestaurantCard key={restro.data.id} restroData={restro} />;
+        })}
       </div>
     </div>
   );
 };
 
 const RestaurantCard = (props) => {
-  //{ resName, cusine } in the parameters OR
-  const { resName, cusine } = props; // Destructuring
-
+  const { restroData } = props;
+  const {
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+    cloudinaryImageId,
+  } = restroData?.data;
   return (
     <div className="restro-card" style={styleCard}>
       <img
         className="restro-logo"
         alt="Burger"
-        src="https://b.zmtcdn.com/data/pictures/chains/3/17953943/816872e9f1fcc43304e21a0f050f5f21_o2_featured_v2.jpg?output-format=webp"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_600/" +
+          cloudinaryImageId
+        }
       />
-      <h3>{resName}</h3>
-      <h4>{cusine}</h4>
-      <h4>4.4 stars</h4>
-      <h4>38 minutes</h4>
+
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>₹{costForTwo / 100} for Two</h4>
+      <h4>{deliveryTime} minutes</h4>
     </div>
   );
 };
