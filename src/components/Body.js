42201156ebdab8f291cbb0d9b87ab7e7c9f9fd6d
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-04-01 11:35:46 -0400

    Implementing useState Hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,30 @@
+import { useState } from "react";
+
 import restroList from "../utils/restroApiData";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  // Local State Variable - Super powerful variable
+  const [listOfRestaurants, setListOfRestaurants] = useState(restroList);
+
+  // Normal JS variable
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="restro-container">
-        {restroList.map((restro) => {
+        {listOfRestaurants.map((restro) => {
           return <RestaurantCard key={restro.data.id} restroData={restro} />;
         })}
       </div>
