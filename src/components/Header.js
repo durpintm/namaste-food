0864a8872f1c1712dad8d1af7d41e4a4e281ea77
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-04-07 17:19:20 -0400

    Add router in the app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import logo from "../../images/logo.webp";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   const [btnName, setBtnName] = useState("Login");
@@ -12,9 +13,15 @@ function Header() {
         </div>
         <div className="nav-items">
           <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
             <li>Cart</li>
             <button
               className="login"
