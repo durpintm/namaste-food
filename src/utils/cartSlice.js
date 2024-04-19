8ffc9e80fb01b792669bf7f6254d52bde88404c3
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-04-19 19:37:06 -0400

    Setting up testing dependencies

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -7,14 +7,22 @@ const cartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      // mutating(modifying) the state here
+      // Vanilla(older) Redux => DON'T MUTATE STATE, returning was mandatory
+      // const newState = [...state];
+      // newState.items.push(action.payload);
+      // return newState;
+
+      // Redux Toolkit => mutating(modifying) the state here
       state.items.push(action.payload);
     },
     removeItem: (state) => {
       state.items.pop();
     },
+    // originalState = ["pizza"]
     clearCart: (state) => {
-      state.items.length = 0;
+      // RTK => either mutate the existing state or return a new state
+      // state.items.length = 0; // originalState = []
+      return { items: [] }; // This new [] will be replaced inside originalState = []
     },
   },
 });
