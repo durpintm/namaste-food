24e0585a292e17ae5a81ba7d9809cfb141a81a07
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-04-19 22:31:51 -0400

    Writing test cases for Contact Us page

---
+++ src/__tests__/Contact.test.js
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../components/Contact";
+import "@testing-library/jest-dom";
+
+describe("Contact Us Page Test Cases", () => {
+  it("Should load headings contact us component", () => {
+    render(<Contact />);
+
+    // const heading = screen.getByRole("heading");
+    const headings = screen.getAllByRole("heading");
+
+    expect(headings.length).toBe(2);
+  });
+
+  test("Should load button inside contact us component", () => {
+    render(<Contact />);
+
+    // const button = screen.getByRole("button");
+    const button = screen.getByText("Submit");
+
+    expect(button).toBeInTheDocument();
+  });
+
+  test("Should load input name inside contact us component", () => {
+    render(<Contact />);
+
+    const inputName = screen.getByPlaceholderText("name");
+
+    expect(inputName).toBeInTheDocument();
+  });
+
+  test("Should load two input boxes in the contact component", () => {
+    render(<Contact />);
+
+    const inputBoxes = screen.getAllByRole("textbox");
+    // console.log(inputBoxes);
+    // expect(inputBoxes.length).toBe(2);
+    expect(inputBoxes.length).not.toBe(4);
+  });
+});
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,25 @@
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
-      <h2>This is Contact Page of Namaste Food</h2>
+      <h1 className="font-bold text-3xl p-4 m-4">Contact</h1>
+      <h2 className="font-bold text-2xl p-4 m-4">
+        This is Contact Page of Namaste Food
+      </h2>
+      <form className="p-4 m-4">
+        <input
+          className="border border-black p-2 m-2"
+          type="text"
+          placeholder="name"
+        ></input>
+        <input
+          className="border border-black p-2 m-2"
+          type="text"
+          placeholder="message"
+        ></input>
+        <button className="border border-black p-2 m-2 bg-gray-200 rounded-lg">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
